e85b2842a808625929be0c585656a5e70186faa1
manish3063 <manishsingh111095@gmail.com>
2022-06-08 16:10:49 +0530

    multiplication

---
+++ multiplication.go
@@ -0,0 +1,12 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	num1 := 10
+	num2 := 22
+	result := num1 * num2
+	fmt.Println("result is: ", result)
+}
